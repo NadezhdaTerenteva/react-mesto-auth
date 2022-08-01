a19d670900287ba05f97696ebb2e39d837f689d4
Nadezhda Terenteva <nadya_09@inbox.ru>
2022-08-02 00:22:03 +0300

    new components created

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,7 +1,28 @@
 import React from "react";
 
 function Login() {
-    
+    return (
+      <div className="login">
+        <h3 className="login__header">Вход</h3>
+        <form className="login__form">
+          <input 
+          className="login__input" 
+          type="email"
+          name="email"
+          placeholder="Email"
+          required>
+          </input>
+          <input 
+          className="login__input" 
+          type="password"
+          name="password"
+          placeholder="Пароль"
+          required>
+          </input>
+          <button className="login__submit-button" type="submit">Войти</button>
+        </form>
+      </div>  
+    )
 }
 
 export default Login;
--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -4,7 +4,7 @@ function Register() {
     return (
     <div className="register">
         <h3 className="register__header">Регистрация</h3>
-        <div className="register__form">
+        <form className="register__form">
           <input 
           className="register__input" 
           type="email"
@@ -20,7 +20,7 @@ function Register() {
           required>
           </input>
           <button className="register__submit-button" type="submit">Зарегистрироваться</button>
-        </div>
+        </form>
         <h4 className="register__caption">Уже зарегистрированы? Войти</h4>
     </div>
     );
